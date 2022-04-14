6c90cce6a21954d8de64ba5665b2e01a5a849b3b
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2022-04-14 18:40:00 -0300

    requisito 3.0

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) return {};
+  const objF = employees.find((n) => n.firstName === employeeName || n.lastName === employeeName);
+  return objF;
 }
 
 module.exports = getEmployeeByName;
