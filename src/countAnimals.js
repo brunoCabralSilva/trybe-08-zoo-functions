6e7d5a313d77631c1ba2be0d2bec5a513b4338b5
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2022-04-16 16:03:57 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,11 +1,25 @@
-// const data = require('../data/zoo_data');
-// const { species } = require('../data/zoo_data');
+const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-// function countAnimals(animals) {
-//   if (animals === undefined) {
-//   }
-// }
+function countAnimals(animals) {
+  if (animals === undefined) {
+    return species.reduce((acc, especie) => {
+    acc[especie.name] = especie.residents.length;
+  return acc;
+    },{});
+  }
+  if (Object.keys(animals).length === 1) {
+    const encontraAnimal = species.find((especie) => especie.name === Object.values(animals)[0]);
+    return encontraAnimal.residents.length;
+  }
+  if (Object.keys(animals).length === 2) {
+    const encontraAnimal = species.find((especie) => especie.name === Object.values(animals)[0]);
+    const { residents } = encontraAnimal;
+    const filtradosPorSexo = residents.filter((animal) => animal.sex === Object.values(animals)[1]);
+    return filtradosPorSexo.length;
+  }
+}
 
-// module.exports = countAnimals;
+console.log(countAnimals({ specie: 'giraffes', sex: 'male' }));
 
-// console.log(countAnimals());
+module.exports = countAnimals;
