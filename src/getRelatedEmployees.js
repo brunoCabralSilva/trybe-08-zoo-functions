ac8d6f186ebd05a9c395b7b741238b6300b3c039
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2022-04-16 16:57:25 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,13 +1,30 @@
-// const data = require('../data/zoo_data');
-// const { employees } = require('../data/zoo_data');
+const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
-// function isManager(id) {
-// }
+function isManager(id) {
+  const arrayAdms = [];
+  const adms = employees.map((valor) => valor.managers);
+  for (let i = 0; i < adms.length; i += 1) {
+    for (let j = 0; j < adms[i].length; j += 1) {
+      arrayAdms.push(adms[i][j]);
+    }
+  }
+  return arrayAdms.some((ids) => ids === id);
+}
 
-// function getRelatedEmployees(managerId) {
-//   // seu código aqui
-// }
+function getRelatedEmployees(managerId) {
+  try {
+    if (isManager(managerId)) {
+      const pessoasSubordinadas = employees.filter((pessoa) => pessoa.managers.includes(managerId));
+      const nC = pessoasSubordinadas.map((n) => `${n.firstName} ${n.lastName}`);
+      return nC;
+    }
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } catch (error) {
+    throw error.message;
+  }
+}
 
-// module.exports = { isManager, getRelatedEmployees };
+module.exports = { isManager, getRelatedEmployees };
 
-// console.log(isManager('b0dc644a-5335-489b-8a2c-4e086c7819a2'));
+console.log(getRelatedEmployees('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
