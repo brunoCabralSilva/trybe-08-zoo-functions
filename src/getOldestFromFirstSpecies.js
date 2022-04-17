2893d22508ba6ee03817be075f88a8b7cdb1069d
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2022-04-17 13:51:07 -0300

    Requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employee = employees.find((valor) => valor.id === id);
+  const primeiroAnimal = employee.responsibleFor.find((valor) => valor[0]);
+  const objAnimal = species.find((valor) => valor.id === primeiroAnimal);
+  const { residents } = objAnimal;
+  const comparaIdade = residents.reduce((acc, idade) => {
+    if (idade.age >= acc) {
+      return idade.age;
+    }
+    return acc;
+  }, 0);
+
+  const encontraMaisVelho = residents.find((valor) => valor.age === comparaIdade);
+  const dadosAnimal = [encontraMaisVelho.name, encontraMaisVelho.sex, encontraMaisVelho.age];
+  return dadosAnimal;
 }
+
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
